defa1118cffaf91b5cf253c7cdfc0348a39d035a
matynjr <akua@AKUAs-MacBook-Pro.local>
2023-08-29 21:49:06 +0300

    more content

---
+++ oldfilecontent.php
@@ -0,0 +1,33 @@
+[sc name="moje-oblibene-vice-2" ]
+[sc name="moje-oblibene-products-3"]
+[sc name="moje-oblibene-products-2"]
+[sc name="moje-oblibene-products"]
+[sc name="moje-oblibene-products-4" ]
+
+<!-- products 2 -->
+<div class="products section-space--ptb_100 border-bottom">
+  <div class="container">    
+    <div class="row">
+      <div class="col-sm-5">
+        <img src="<?php echo get_stylesheet_directory_uri()?>/assets/images/blog/prod4.png" class="mx-auto">
+      </div>
+      <div class="col-sm-7  my-auto">
+        <div class="content-wrap">
+          <div class="content">
+            <div class="main-content text-center">
+              <h3 class="font-weight--reguler mb-30">Akční plán proti akné – 6 věcí, které můžete pro svou pleť udělat ještě dnes</h3>
+              <p>Chceš začít měnit svoji pleť co nejdřív, ale nevíš, kde začít? Stáhni si Akční plán proti akné a získej 6 jednoduchých kroků, které můžeš pro svou pleť udělat ještě dnes.</p>
+              <div class="row">
+                <div class="hero-button mt-30 mb-30 mx-auto">
+        <button onclick="location.href='https://www.krasnazevnitr.cz/ackni_plan_proti_akne'" class="ht-btn ht-btn-md text-uppercase rounded-0">Stáhnout zdarma!</button>
+      </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</div>
+<!-- products 2 -->
